06cbfa17559a41b28cd16dfd0f8fcb387c7be11b
yusuketomoto <yukmpkt@gmail.com>
2015-08-18 21:54:40 +0900

    Add description getters

--- src/ofxNatNet.h
+++ src/ofxNatNet.h
@@ -152,6 +152,10 @@ public:
 	void debugDrawInformation();
 	void debugDrawMarkers();
     
+    inline const vector<MarkerSetDescription> getMarkerSetDescriptions() { return markerset_descs; }
+    inline const vector<RigidBodyDescription> getRigidBodyDescriptions() { return rigidbody_descs; }
+    inline const vector<SkeletonDescription> getSkeletonDescriptions() { return skeleton_descs; }
+    
 protected:
 	InternalThread* thread;
 
